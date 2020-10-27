f27e7fd1c242dfd4dc873068241255df0e2e6f53
Miguel Perez <mapper.895.mp@gmail.com>
2020-10-27 12:43:15 -0600

    for script

---
+++ for.py
@@ -0,0 +1,27 @@
+"""Clase de For."""
+
+"""
+names = ['Abraham', 'Cesar', 'Daniel', 'Daniel', 'Diego', 'Edgar']
+
+for name in names:
+	print(f'Student: {name}')
+else:
+	print('No more names')
+"""
+
+"""
+string = 'Miguel'
+
+for char in string:
+	if char != 'i':
+		print(char)
+	else:
+		print('Out of the for')
+"""
+
+numbers = []
+
+for number in range(0,21,2):
+	numbers.append(number)
+
+print(numbers)
